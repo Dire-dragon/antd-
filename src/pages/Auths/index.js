5371ef4bb89b5bffec94f41173bcf6ba46f9ce13
Dire-dragon <1536175744@qq.com>
2021-10-27 12:56:31 +0800

    权限界面

--- src/pages/Auths/index.js
+++ src/pages/Auths/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Card, Breadcrumb, Layout } from 'antd';
+import React, { useState, useEffect } from 'react';
+import {
+  Card,
+  Breadcrumb,
+  Layout,
+  Form,
+  Input,
+  Button,
+  Table,
+  Pagination,
+} from 'antd';
 /**
  * 权限管理接口
  *
@@ -10,10 +19,94 @@ import { Card, Breadcrumb, Layout } from 'antd';
  *  msg:  返回信息
  */
 import { getAuthList, editAuth, addAuth } from '../../api/auth/index';
+import { PlusOutlined } from '@ant-design/icons';
 const { Content } = Layout;
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: '名称',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: '操作',
+  },
+];
+
+const PAGE = {
+  PAGETOTAL: 0,
+  PAGEPER: 5,
+};
+
+function useAuthList() {
+  const [initAuthList, setInitAuthList] = useState();
+
+  useEffect(() => {
+    getInitAuthList();
+  }, []);
+
+  const getInitAuthList = async () => {
+    const authList = await getAuthList();
+    setInitAuthList(authList.data);
+    console.log(initAuthList);
+  };
+
+  return [initAuthList];
+}
 
 function NewService1() {
   // console.log(getAuthList());
+  const [initAuthList] = useAuthList();
+
+  console.log(initAuthList);
+
+  const authSerachForm = (
+    <div>
+      <Form layout={'inline'}>
+        <Form.Item label="ID：">
+          <Input />
+        </Form.Item>
+        <Form.Item label="名称：">
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary">查询</Button>
+          <Button>重置</Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+
+  const authAddBtn = (
+    <div>
+      <Button type="primary" icon={[<PlusOutlined />]}>
+        新建
+      </Button>
+    </div>
+  );
+
+  const authTable = (
+    <div>
+      <Table
+        pagination={<Pagination total={50} />}
+        dataSource={initAuthList}
+        columns={columns}
+      ></Table>
+    </div>
+  );
+
+  const authListPage = <div></div>;
+
   return (
     <Card>
       <Breadcrumb separator=">">
@@ -22,7 +115,10 @@ function NewService1() {
       </Breadcrumb>
 
       <Content style={{ margin: '15px 0' }}>
-        <p>权限管理内容</p>
+        {authSerachForm}
+        {authAddBtn}
+        {authTable}
+        {authListPage}
       </Content>
     </Card>
   );
